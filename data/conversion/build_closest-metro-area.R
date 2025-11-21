fb9abee112c9003a7ea678e9fbe0aa972b67ed63
twallema <tallema2@jh.edu>
2025-11-20 20:04:03 -0500

    works

--- data/conversion/build_closest-metro-area.R
+++ data/conversion/build_closest-metro-area.R
@@ -5,11 +5,12 @@ library(ggplot2)
 library(tidyverse)
 library(randomcoloR)
 
-# Download shape files of states, municipalities
+# Paths relative to this file
+setwd(dirname(rstudioapi::getActiveDocumentContext()$path))
+
+# Download shape files of states, municipalities and metro areas
 shape_states = geobr::read_state(year=2018)
 shape_munis = read_municipality(year=2018)
-
-# Download metro areas and urban areas & visualise on map
 metros <- geobr::read_metro_area(
   year = 2018,
   code_state = "all",
@@ -22,11 +23,11 @@ metro_polys <- metros %>%
   group_by(name_metro) %>%
   summarise(geometry = st_union(geom)) %>%
   st_as_sf()
-# Take out imperfections
+# Filter imperfections
 metro_polys <- metro_polys %>%
   mutate(geometry = st_simplify(geometry, dTolerance = 100))
 
-# Find which metros share a border
+# Find which metro areas share a border
 touch_list <- st_intersects(metro_polys)
 g <- graph_from_adj_list(touch_list)
 clusters <- components(g)$membership
@@ -37,64 +38,50 @@ metro_polys <- metro_polys %>%
   group_by(cluster_id) %>%
   summarise(geometry = st_union(geometry)) %>%
   st_as_sf()
-# Take out imperfections
+# Filter out imperfections
 metro_polys <- metro_polys %>%
   mutate(geometry = st_simplify(geometry, dTolerance = 100))
 
-# Compute hypermetro area centroid 
-metro_centroids <- metro_polys %>% 
-  st_transform(3857)
-metro_centroids <- st_centroid(metro_centroids)
-
-# Find out what municipalities are not in a metro area
-all_munis <- shape_munis %>% st_transform(3857)
-munis_not_metro <- all_munis %>%
-  filter(!(code_muni %in% metros$code_muni))
-
-# Find out what municipalities are in a metro area
-munis_metro <- all_munis %>%
-  filter(code_muni %in% metros$code_muni)
-
-# Compute the distance from each non metro municipality to the hypermetro areas
-dist_matrix <- st_distance(munis_not_metro, metro_centroids)
-
-# Find the nearest hypermetro area
-nearest_idx <- apply(dist_matrix, 1, which.min)
-munis_not_metro$nearest_hypermetro_id <- metro_centroids$cluster_id[nearest_idx]
-
-### Visualise nearest hypermetro areas
-n_cols <- length(unique(munis_not_metro$nearest_hypermetro_id))
-cols <- randomColor(n_cols, luminosity = "bright")
-ggplot() + theme_void() + theme_bw() + 
-  ggtitle("Urban clusters") +
-  geom_sf(data = shape_states, color="black") +
-  geom_sf(data = metro_polys, color='grey') +
-  geom_sf(data = munis_not_metro, aes(fill=factor(nearest_hypermetro_id)), alpha=1) +
-  scale_fill_manual(values = cols) +
-  theme(legend.position = "none") #+
-#coord_sf(xlim = c(-50, -40), ylim = c(-25, -20))
-
-
-
 # Compute final result
-metro_polys2 <- st_transform(metro_polys, 3857)
-munis2 <- st_transform(shape_munis, 3857)
+## Distance based coordinates
+metro_transform <- st_transform(metro_polys, 3857)
+munis_transform <- st_transform(shape_munis, 3857)
+## Use nearest feature (so not centroid distance!)
+nearest_idx <- st_nearest_feature(munis_transform, metro_transform)
+result <- munis_transform %>%
+  mutate(cluster_id = metro_transform$cluster_id[nearest_idx])
+## Save result
+result_no_geom <- st_drop_geometry(result)
+result_no_geom <- result_no_geom %>% rename("hypermetro_id" = "cluster_id")
+folder <- "../interim/nearest-hypermetro"
+if (!dir.exists(folder)) {
+  dir.create(folder, recursive = TRUE)
+}
+write.csv(result_no_geom[, c("code_muni", "hypermetro_id")],
+          file.path(folder, "nearest-hypermetro_mun.csv"),
+          row.names = FALSE)
 
-nearest_idx <- st_nearest_feature(munis2, metro_polys2)
-munis_in_hypermetro <- munis2 %>%
-  mutate(cluster_id = metro_polys2$cluster_id[nearest_idx])
 
-### Visualise nearest hypermetro areas
-n_cols <- length(unique(munis_in_hypermetro$cluster_id))
+## Visualise result
+### Dissolve municipalities
+result <- result %>%
+  group_by(cluster_id) %>%
+  summarise(geom = st_union(geom)) %>%
+  st_as_sf()
+### Filter imperfections
+result <- result %>%
+  mutate(geom = st_simplify(geom, dTolerance = 100))
+### Make plot
+n_cols <- length(unique(result$cluster_id))
 cols <- randomColor(n_cols, luminosity = "bright")
 ggplot() + theme_void() + theme_bw() + 
-  ggtitle("Urban clusters") +
-  geom_sf(data = shape_states, color="black") +
-  geom_sf(data = metro_polys, color='grey') +
-  geom_sf(data = munis_in_hypermetro, aes(fill=factor(cluster_id)), alpha=0.5) +
+  ggtitle("Nearest hypermetro area") +
+  geom_sf(data = result, aes(fill=factor(cluster_id)), alpha=1) +
+  geom_sf(data = metro_polys, color='black', alpha=0.8) +
   scale_fill_manual(values = cols) +
-  theme(legend.position = "none") #+
-#coord_sf(xlim = c(-50, -40), ylim = c(-25, -20))
+  theme(legend.position = "none")
+### Save the plot
+ggsave(file.path(folder,"nearest-hypermetro.pdf"))
 
 
 
