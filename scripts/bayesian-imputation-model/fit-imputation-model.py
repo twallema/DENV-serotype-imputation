76fde3f42d449fb49a876eb53a482d8f243f70b7
twallema <tallema2@jh.edu>
2025-12-07 13:28:06 -0500

    don't do adapt_full --> uses 121Gb ram

--- scripts/bayesian-imputation-model/fit-imputation-model.py
+++ scripts/bayesian-imputation-model/fit-imputation-model.py
@@ -283,7 +283,7 @@ with pm.Model() as model:
 # NUTS
 draws=500
 with model:
-    trace = pm.sample(draws, tune=1500, target_accept=0.99, chains=chains, cores=chains, init='adapt_full', progressbar=True, idata_kwargs={'log_likelihood':True})
+    trace = pm.sample(draws, tune=1500, target_accept=0.99, chains=chains, cores=chains, init='adapt_diag', progressbar=True, idata_kwargs={'log_likelihood':True})
 
 
 #######################
