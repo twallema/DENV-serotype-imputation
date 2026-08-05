b42960fad7a974284b80f9e0fff559768f9d2a3d
twallema <twa27@cornell.edu>
2026-08-05 15:49:35 -0400

    long runs for real

--- data/conversion/construct_travel-time-matrices.R
+++ data/conversion/construct_travel-time-matrices.R
@@ -7,10 +7,10 @@ library(dplyr)
 # Fetch the OpenStreetMap for Brazil
 # https://download.geofabrik.de/south-america/brazil.html --> Download brazil-latest.osm.pbf
 
-geo_level <- "CD_RGI"
+geo_level <- "CD_RGINT"
 year = 2022
-N_runs <- 2 # Number of random municipality pairs to sample per origin-destination pair
-max_attempts <- 2
+N_runs <- 500 # Number of random municipality pairs to sample per origin-destination pair
+max_attempts <- 10
 travel_duration_threshold <- 4*24   # use maximum trip length of one week to discard ferry trips along Amazon
 
 # Amazon region and how it connects to Manaus requires special care
