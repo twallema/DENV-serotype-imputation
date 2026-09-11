9daded30d5ecd0739bb19d1cd46efd39bf0dbe15
twallema <twa27@cornell.edu>
2026-09-11 15:54:51 -0400

    make seneca submission files

---
+++ scripts/clustering/seneca_hypersubmission.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+run_id="CD_RGINT_v4_mean"
+spatial_aggregation="rgint"
+
+N=50
+threads=8
+time="100:00:00"
+
+for i in $(seq 1 "$N"); do
+
+    repeat_id="$i"
+
+    echo "Submitting repeat ${repeat_id} of run ID '${run_id}'"
+
+    job_id=$(sbatch --parsable \
+        -c "$threads" \
+        --time="$time" \
+        --job-name="${run_id}_repeat_${repeat_id}" \
+        seneca_submit_hyperoptimise-clusters_single.sh \
+        "$run_id" \
+        "$repeat_id" \
+        "$threads" \
+        "$spatial_aggregation")
+
+done
---
+++ scripts/clustering/seneca_submit_hyperoptimise-clusters_single.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+#SBATCH --account=arb24_0001
+#SBATCH --partition=cac_cpu
+#SBATCH --qos=longrun
+
+# The IDs and number of threads
+run_id=$1
+repeat_id=$2
+threads=$3
+spatial_aggregation=$4
+
+echo "Running repeat ${repeat_id} of clustering hyperoptimisation with ID '${run_id}'"
+
+# Load Anaconda
+module load anaconda3
+
+# Activate conda environment
+source /opt/ohpc/pub/software/anaconda3/etc/profile.d/conda.sh
+conda activate DENV-SEROTYPE-IMPUTATION
+
+unset PYTHONHOME
+unset PYTHONPATH
+
+# Run Python script
+python hyperoptimise-clusters.py \
+    --n_cores "${threads}" \
+    --n_maxp 250 \
+    --max_iterations_sa 10 \
+    --spatial_aggregation "${spatial_aggregation}" \
+    --validation_n 279 \
+    --run_id "${run_id}" \
+    --repeat_id "${repeat_id}"
+
+# Deactivate environment
+conda deactivate
